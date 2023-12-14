aa7255fea57ab59c533c6e1c9de78fd5126e71a8
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-14 06:21:41 +0000

    improve logic within reduce

--- merge-alternately.ts
+++ merge-alternately.ts
@@ -1,19 +1,20 @@
 function mergeAlternately(word1: string, word2: string): string {
-    const largerWord = Math.max(word1.length, word2.length);
 
-let mergedArr = [""];
-
-for (let i = 0; i < largerWord; i++) {
-    if (word1[i]) {
-    mergedArr.push(word1[i]);
-    }
-        if (word2[i]) {
-    mergedArr.push(word2[i]);
-    }
-}
-
-return mergedArr.join("");
-};
+    const mergedArr = word1.split("").reduce((merged: string[], current, index) => {
+        if (word2[index]) {
+            merged.push(word1[index]);
+            merged.push(word2[index]);
+            } else {
+                merged.push(word1[index]);
+                }
+        if (!word1[index+1]) merged.push(word2.slice(index+1));
+        return merged;
+    }, []);
+    
+    
+    
+    return mergedArr.flat().join("");
+    };
    
    console.log(mergeAlternately("abc", "pqr"));
    console.log(mergeAlternately("ab", "pqrs"));
