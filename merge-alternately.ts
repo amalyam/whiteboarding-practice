0b89ff8f49eb54ff877bb949d98cf18eb79acbe3
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-14 05:34:34 +0000

    make more efficient by concatenating to array

--- merge-alternately.ts
+++ merge-alternately.ts
@@ -1,18 +1,18 @@
 function mergeAlternately(word1: string, word2: string): string {
     const largerWord = Math.max(word1.length, word2.length);
 
-let mergedString = "";
+let mergedArr = [""];
 
 for (let i = 0; i < largerWord; i++) {
     if (word1[i]) {
-    mergedString += word1[i];
+    mergedArr.push(word1[i]);
     }
         if (word2[i]) {
-    mergedString += word2[i];
+    mergedArr.push(word2[i]);
     }
 }
 
-return mergedString;
+return mergedArr.join("");
 };
    
    console.log(mergeAlternately("abc", "pqr"));
