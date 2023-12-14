cd7dbb9ca7b0197fdab1f669c1655595fb8f3852
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-14 05:27:00 +0000

    solution 3

--- merge-alternately.ts
+++ merge-alternately.ts
@@ -1,10 +1,11 @@
 function mergeAlternately(word1: string, word2: string): string {
     const wordArr1 = word1.split("");
     const wordArr2 = word2.split("");
+    const largerWord = Math.max(wordArr1.length, wordArr2.length);
    
    let mergedArr = [];
    
-   for (let i = 0; i < wordArr1.length + wordArr2.length; i++) {
+   for (let i = 0; i < largerWord; i++) {
        if (wordArr1[i]) {
        mergedArr.push(wordArr1[i]);
        }
