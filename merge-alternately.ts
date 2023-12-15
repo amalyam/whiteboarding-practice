90ecc6954ea67384eaba24bc01df02f2cf9c936a
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-15 17:47:01 +0000

    replace word1[index] with current

--- merge-alternately.ts
+++ merge-alternately.ts
@@ -2,10 +2,10 @@ function mergeAlternately(word1: string, word2: string): string {
 
     const mergedArr = word1.split("").reduce((merged: string[], current, index) => {
         if (word2[index]) {
-            merged.push(word1[index]);
+            merged.push(current);
             merged.push(word2[index]);
             } else {
-                merged.push(word1[index]);
+                merged.push(current);
                 }
         if (!word1[index+1]) merged.push(word2.slice(index+1));
         return merged;
