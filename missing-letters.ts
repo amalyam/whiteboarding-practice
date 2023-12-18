2aa1956728d628b166270205b38e02fb5a138c2f
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-18 14:44:40 +0000

    solution 1

--- missing-letters.ts
+++ missing-letters.ts
@@ -11,3 +11,23 @@ fearNotLetter("bcdf") should return the string e.
 fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
 
 */
+
+function fearNotLetter(str: string) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const startingIndex = alphabet.search(str[0]);
+
+    for (let i = 0; i < str.length; i++) {
+        const alphabetIndex = startingIndex + i
+        if (str[i] !== alphabet[alphabetIndex]) {
+            return alphabet[alphabetIndex];
+        }
+    }
+    
+  }
+  
+
+console.log(fearNotLetter("abce")); // should return the string d.
+console.log(fearNotLetter("abcdefghjklmno")); // should return the string i.
+console.log(fearNotLetter("stvwx")); // should return the string u.
+console.log(fearNotLetter("bcdf")); // should return the string e.
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz")); // should return undefined.
