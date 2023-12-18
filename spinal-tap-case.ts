473f38d7d80dcda22c1e9c273e88369d33fd3ee4
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2023-12-18 14:32:18 +0000

    solution 1

--- spinal-tap-case.ts
+++ spinal-tap-case.ts
@@ -10,3 +10,30 @@ spinalCase("The_Andy_Griffith_Show") should return the string the-andy-griffith-
 spinalCase("Teletubbies say Eh-oh") should return the string teletubbies-say-eh-oh.
 spinalCase("AllThe-small Things") should return the string all-the-small-things.
 */
+
+function spinalCase(str: string) {
+    const strArray = str.split("");
+
+    return strArray.reduce((spinalString, current, index) => {
+      if ((/[A-Z]/.test(current))) {
+        spinalString += current.toLowerCase();
+      } else if ((/[a-z]/.test(current))) {
+        spinalString += current;
+      }
+
+      if (index + 1 !== strArray.length && current === " " || current === "_" || current === "-" || (/[A-Z]/.test(strArray[index+1]))) {
+        spinalString += "-";
+      }
+
+      return spinalString;
+    }, "");
+  }
+
+
+  
+
+console.log(spinalCase("This Is Spinal Tap")); //this-is-spinal-tap
+console.log(spinalCase("thisIsSpinalTap")); //this-is-spinal-tap
+console.log(spinalCase("The_Andy_Griffith_Show")); //the-andy-griffith-show
+console.log(spinalCase("Teletubbies say Eh-oh")); //teletubbies-say-eh-oh
+console.log(spinalCase("AllThe-small Things")); //all-the-small-things
